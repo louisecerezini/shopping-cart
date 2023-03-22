9870dc45979a1bb95bb9f7e52a7a5b540eda71b0
louisecerezini <nutrilouiserodrigues@gmail.com>
2023-03-22 18:42:55 -0300

    arrumando o cy

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,11 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-const fetchProdutos = (produto) => fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produto}`);
+const fetchProdutos = (produto) =>
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produto}`);
 
 export const fetchProductsList = async (produto) => {
   if (!produto) {
-    return Promise.reject(new Error('Tnermo de busca não informado'));
+    return Promise.reject(new Error('Termo de busca não informado'));
   }
   try {
     const response = await fetchProdutos(produto);
