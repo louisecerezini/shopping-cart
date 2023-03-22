262cb211d3bfdad920544cecf7a6374014509b42
louisecerezini <nutrilouiserodrigues@gmail.com>
2023-03-22 18:26:20 -0300

    arrumando o lint

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,19 +2,17 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-const fetchProdutos = (produto) => fetch (`https://api.mercadolibre.com/sites/MLB/search?q=${produto}`)
+const fetchProdutos = (produto) => fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produto}`);
 
 export const fetchProductsList = async (produto) => {
   if (!produto) {
-    return Promise.reject(new Error('Termo de busca não informado'));
+    return Promise.reject(new Error('Tnermo de busca não informado'));
   }
-  try{
-  const response = await fetchProdutos(produto);
-  const data = await response.json(); 
-  return data.results; 
-} catch (error) {
-  return error.message;
-}
-}
-
-
+  try {
+    const response = await fetchProdutos(produto);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    return error.message;
+  }
+};
