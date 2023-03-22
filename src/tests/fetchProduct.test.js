9a3bbee1e0d8abaf0f8cf152b4c43fa514c386d2
louisecerezini <nutrilouiserodrigues@gmail.com>
2023-03-22 18:20:27 -0300

    adicionando primeiro e segundo requisito

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -1,10 +1,9 @@
 import './mocks/fetchSimulator';
-import { fetchProduct } from '../helpers/fetchFunctions';
+import { fetchProduct, fetchProductsList } from '../helpers/fetchFunctions';
 import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+ 
+  
   });
-});
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,29 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-  expect
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith(
+      'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    );
   });
 
-  // it('...', () => {
-  // });
+  it(`retorno da função fetchProductsList com o argumento 'computador' é uma estrutura de dados igual ao objeto computadorSearch`, async () => {
+    const atual = await fetchProductsList('computador');
+    expect(atual).toEqual(computadorSearch);
+  });
+
+  it('ao chamar a funcao fetchProductsList sem argumento, retorna um erro com a mensagem', async () => {
+    await expect(fetchProductsList()).rejects.toThrow(
+      new Error('Termo de busca não informado')
+    );
+  });
 });
