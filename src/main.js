44be54cc290c3317265d1d585512ebddaa9e32b2
louisecerezini <nutrilouiserodrigues@gmail.com>
2023-03-23 12:12:37 -0300

    adicionando o requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const productsElement = document.querySelector('.products');
+
+window.onload = (async () => {
+  const produtos = await fetchProductsList('computador');
+  produtos.forEach((product) => {
+    productsElement.appendChild(createProductElement(product));
+  });
+  // console.log(produtos);
+});
