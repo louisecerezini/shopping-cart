e7e26679d602a5de0fe0d215ba188c3933f3a901
louisecerezini <nutrilouiserodrigues@gmail.com>
2023-03-27 11:28:48 -0300

    adicionando o requisito 8

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
+import { saveCartID } from './helpers/cartFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const productsElement = document.querySelector('.products');
 const container = document.querySelector('.container');
 const cart = document.querySelector('.cart');
+const cartElementList= document.querySelector('.cart__products');
 
 const carregar = document.createElement('p');
 const error = document.createElement('p');
@@ -14,11 +16,25 @@ carregar.innerText = 'carregando...';
 carregar.className = 'loading';
 container.appendChild(carregar);
 
+
+const saveCartIdCallback = async (productId) => {
+  const product= await fetchProduct(productId);
+  saveCartID(productId);
+  const cartElement = createCartProductElement(product);
+  cartElementList.appendChild(cartElement); 
+}
+
 window.onload = async () => {
   try {
     const produtos = await fetchProductsList('computador');
     produtos.forEach((product) => {
-      productsElement.appendChild(createProductElement(product));
+      // cria a caixa toda com o elemento que representa o produto
+      const productElement = createProductElement(product); 
+      //pega o botao da caixa
+      let buttonElement = productElement.querySelector('.product__add');
+      //colocar o listener no onclick do botao passando como parametro o callback contendo uma chamada para o savecartid com o id do produto
+      buttonElement.addEventListener('click',() => saveCartIdCallback(product.id)); 
+      productsElement.appendChild(productElement);
     });
     container.removeChild(carregar);
     error.remove();
